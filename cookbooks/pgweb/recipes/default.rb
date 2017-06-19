50dd3dca4b0e830c1970416f0dac92774d79fe9f
Kannan R <kannaa.ram@gmail.com>
2017-06-19 21:02:33 +0530

    Fixes permission issues and service disabled.

--- cookbooks/pgweb/recipes/default.rb
+++ cookbooks/pgweb/recipes/default.rb
@@ -6,28 +6,11 @@
 
 cookbook_file '/usr/bin/pgweb' do
   source 'pgweb'
-  owner 'root'
-  group 'root'
-  mode  '0700'
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode  '0755'
   action :create
 end
-
-# remote_file "/data/pgweb.zip" do
-#   source "https://github.com/sosedoff/pgweb/releases/download/v#{node[:pgweb][:version]}/pgweb_linux_amd64.zip"
-# end
-
-# execute 'extract the pgweb zip' do
-#   command 'cd /data; unzip -u pgweb.zip'
-#   cwd '/data/'
-#   only_if { File.exists?("/data/pgweb.zip") }
-# end
-#
-# remote_file "/usr/bin/pgweb" do
-#   source "file:///data/pgweb_linux_amd64"
-#   owner 'root'
-#   group 'root'
-#   mode 0700
-# end
 
 directory '/home/deploy/.pgweb/bookmarks' do
   recursive true
@@ -38,21 +21,7 @@ template '/home/deploy/.pgweb/bookmarks/wellthie.toml' do
   mode '0644'
 end
 
-# template '/etc/systemd/system/pgweb.service' do
-#   source 'pgweb.service.erb'
-# end
-
-template '/etc/init.d/pgweb' do
-  source 'pgweb.init.d.erb'
-end
-
-execute 'adding pgweb to service start' do
-  command 'sudo rc-update add pgweb default'
-end
-
 execute 'start pgweb' do
-  command 'sudo /etc/init.d/pgweb start'
+  command 'nohup /engineyard/bin/pgweb  --bind=0.0.0.0 --listen=8089 --bookmarks-dir=/home/deploy/.pgweb/bookmarks/ --bookmark=wellthie --readonly -s > /dev/null 2>&1 &'
+  user 'deploy'
 end
-# execute 'reload systemctl' do
-#   command 'sudo systemctl daemon-reload && sudo systemctl enable pgweb.service'
-# end
